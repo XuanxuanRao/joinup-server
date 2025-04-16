bb04ca7e50564e800a780acf2ad142f8f71d4efe
XuanxuanRao <18060187175@163.com>
2025-04-16 19:41:52 +0800

    [feat]: 允许跨域

--- joinup-common/src/main/java/cn/org/joinup/common/config/MvcConfig.java
+++ joinup-common/src/main/java/cn/org/joinup/common/config/MvcConfig.java
@@ -4,6 +4,7 @@ import cn.org.joinup.common.interceptors.UserInfoInterceptor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,5 +18,14 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new UserInfoInterceptor());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
