8fae64dac3fdf78c46ac66f952ce43b5ea39e654
XuanxuanRao <18060187175@163.com>
2025-05-05 17:04:14 +0800

    [fix]: 修复修改用户信息时sso密码为空导致的异常

--- user-service/src/main/java/cn/org/joinup/user/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/cn/org/joinup/user/service/impl/UserServiceImpl.java
@@ -244,10 +244,10 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         User user = BeanUtil.copyProperties(updateUserDTO, User.class);
         user.setId(UserContext.getUser());
         user.setUpdateTime(LocalDateTime.now());
+        user.setSsoPassword(null);
         Optional.ofNullable(updateUserDTO.getSsoPassword())
                 .filter(StrUtil::isNotBlank)
                 .ifPresent(pwd -> user.setSsoPassword(PasswordUtil.encrypt(pwd)));
-        System.out.println(user);
         if (!updateById(user)) {
             return Result.error("更新用户信息失败，请稍后再试");
         }
