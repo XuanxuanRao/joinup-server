51ee41af65d49a4d77485e0c28d5dc9b29d69e55
aron <arongao123@gmail.com>
2025-05-18 15:48:05 +0800

    [feat]: 管理员反馈管理

--- message-center/src/main/java/cn/org/joinup/message/controller/AdminFeedbackController.java
+++ message-center/src/main/java/cn/org/joinup/message/controller/AdminFeedbackController.java
@@ -1,11 +1,18 @@
 package cn.org.joinup.message.controller;
 
-import cn.org.joinup.common.result.PageResult;
 import cn.org.joinup.common.result.Result;
-import cn.org.joinup.message.domain.vo.FeedbackVO;
+import cn.org.joinup.message.domain.po.Feedback;
+import cn.org.joinup.message.service.IAdminFeedbackService;
 import cn.org.joinup.message.service.IFeedbackService;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author chenxuanrao06@gmail.com
@@ -16,12 +23,66 @@ import org.springframework.web.bind.annotation.*;
 public class AdminFeedbackController {
 
     private final IFeedbackService feedbackService;
+    private final IAdminFeedbackService iAdminFeedbackService;
 
+    @GetMapping("/count")
+    public Result<Long> count() {
+        Long count = iAdminFeedbackService.count();
+        return Result.success(count);
+    }
+
+    // 分页加载数据
     @GetMapping("/list")
-    public Result<PageResult<FeedbackVO>> query(@RequestParam(required = false) Boolean handled,
-                                                @RequestParam(defaultValue = "1") int pageNum,
-                                                @RequestParam(defaultValue = "10") int pageSize) {
-        return feedbackService.pageQuery(handled, pageNum, pageSize);
+    public IPage<Feedback> list(Pageable pageable) {
+        return iAdminFeedbackService.getPageFeedbacks(pageable);
+    }
+
+    @DeleteMapping("/delete")
+    public Result<Void> delete(@RequestBody Map<String, Long> body) {
+        Long id = body.get("id");
+        iAdminFeedbackService.removeById(id);
+        return Result.success();
+    }
+
+    @DeleteMapping("/delete/batch")
+    public Result<Void> deleteBatch(@RequestBody Map<String, List<Long>> body) {
+        List<Long> ids = body.get("ids");
+        iAdminFeedbackService.removeByIds(ids);
+        return Result.success();
+    }
+
+    @GetMapping("/querySearch")
+    public Result<List<Feedback>> querySearch(@RequestParam String name) {
+        List<Feedback> feedbacks = iAdminFeedbackService.list();
+        List<Feedback> result = new ArrayList<>();
+        for (Feedback feedback : feedbacks) {
+            if (feedback.getContent().contains(name)) {
+                result.add(feedback);
+            }
+        }
+
+        return Result.success(result);
+    }
+
+    // 返回模糊查询数量
+    @GetMapping("/searchCount")
+    public Result<Long> searchCount(@RequestParam String name) {
+        List<Feedback> feedbacks = iAdminFeedbackService.list();
+        Long count = 0L;
+        for (Feedback feedback : feedbacks) {
+            if (feedback.getContent().contains(name)) {
+                count++;
+            }
+        }
+        return Result.success(count);
+    }
+
+    // 分页显示模糊查询结果
+    @GetMapping("/search")
+    public IPage<Feedback> search(@RequestParam String name,
+                                      @RequestParam("page") int page, @RequestParam("size") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return iAdminFeedbackService.getPageFeedbacksSearch(name, pageable);
     }
 
     @PostMapping("/{feedbackId}/handle")
---
+++ message-center/src/main/java/cn/org/joinup/message/service/impl/AdminFeedbackServiceImpl.java
@@ -0,0 +1,34 @@
+package cn.org.joinup.message.service.impl;
+
+import cn.org.joinup.message.domain.po.Feedback;
+import cn.org.joinup.message.mapper.FeedbackMapper;
+import cn.org.joinup.message.service.IAdminFeedbackService;
+import com.alibaba.nacos.common.utils.StringUtils;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AdminFeedbackServiceImpl extends ServiceImpl<FeedbackMapper, Feedback> implements IAdminFeedbackService {
+
+    @Override
+    public IPage<Feedback> getPageFeedbacks(Pageable pageable) {
+        Page<Feedback> page = new Page<>(pageable.getPageNumber(), pageable.getPageSize());
+        return this.page(page);
+    }
+
+    @Override
+    public IPage<Feedback> getPageFeedbacksSearch(String name, Pageable pageable) {
+        Page<Feedback> page = new Page<>(pageable.getPageNumber(), pageable.getPageSize());
+
+        QueryWrapper<Feedback> wrapper = new QueryWrapper<>();
+        wrapper.like(StringUtils.isNotBlank(name), "content", name);
+
+        return this.baseMapper.selectPage(page, wrapper);
+    }
+}
