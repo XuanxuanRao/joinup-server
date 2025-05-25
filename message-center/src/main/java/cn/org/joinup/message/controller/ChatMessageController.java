be6fca26b1c6100642beff07655007115003d617
XuanxuanRao <18060187175@163.com>
2025-05-25 22:09:25 +0800

    [feat]: 优化获取历史消息接口性能

--- message-center/src/main/java/cn/org/joinup/message/controller/ChatMessageController.java
+++ message-center/src/main/java/cn/org/joinup/message/controller/ChatMessageController.java
@@ -1,11 +1,13 @@
 package cn.org.joinup.message.controller;
 
+import cn.org.joinup.api.dto.BriefConversationDTO;
 import cn.org.joinup.api.dto.ChatMessageVO;
 import cn.org.joinup.common.result.PageResult;
 import cn.org.joinup.common.result.Result;
 import cn.org.joinup.common.util.UserContext;
 import cn.org.joinup.message.domain.po.ChatMessage;
 import cn.org.joinup.message.service.IChatMessageService;
+import cn.org.joinup.message.service.IConversationService;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +25,7 @@ import java.util.stream.Collectors;
 public class ChatMessageController {
 
     private final IChatMessageService chatMessageService;
+    private final IConversationService conversationService;
 
     @GetMapping("/{conversationId}")
     public Result<PageResult<ChatMessageVO>> list(@PathVariable String conversationId,
@@ -32,10 +35,17 @@ public class ChatMessageController {
         queryWrapper.eq(ChatMessage::getConversationId, conversationId);
         queryWrapper.orderByDesc(ChatMessage::getCreateTime);
 
+        BriefConversationDTO conversation = conversationService.getBriefConversation(conversationId, UserContext.getUser());
+
         Page<ChatMessage> page = chatMessageService.page(new Page<>(pageNumber, pageSize), queryWrapper);
         List<ChatMessageVO> collect = page.getRecords()
                 .stream()
-                .map(chat -> chatMessageService.convertToVO(chat, UserContext.getUser(), true)).collect(Collectors.toList());
+                .map(chat -> {
+                    ChatMessageVO vo = chatMessageService.convertToVO(chat, UserContext.getUser(), false);
+                    vo.setConversation(conversation);
+                    return vo;
+                })
+                .collect(Collectors.toList());
         return Result.success(PageResult.of(page, collect));
     }
 
