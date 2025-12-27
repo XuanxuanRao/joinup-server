649e3ec457e3bb8f45edfbeed7a6761ac09fc71d
rcx <chenxuanrao06@gmail.com>
2025-12-27 14:42:12 +0800

    fix: override getScheduleByDate to fit ScheduleTask.

--- course-service/src/main/java/cn/org/joinup/course/service/impl/CourseServiceImpl.java
+++ course-service/src/main/java/cn/org/joinup/course/service/impl/CourseServiceImpl.java
@@ -6,7 +6,6 @@ import cn.org.joinup.common.result.PageQuery;
 import cn.org.joinup.common.result.PageResult;
 import cn.org.joinup.common.result.Result;
 import cn.org.joinup.course.config.CrawlerConfig;
-import cn.org.joinup.course.domain.dto.CourseQueryDTO;
 import cn.org.joinup.course.domain.vo.ScheduleVO;
 import cn.org.joinup.course.domain.po.SignLog;
 import cn.org.joinup.course.mapper.SignLogMapper;
@@ -15,9 +14,7 @@ import cn.org.joinup.course.service.ICourseService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -33,13 +30,7 @@ import java.util.Map;
 public class CourseServiceImpl extends ServiceImpl<SignLogMapper, SignLog> implements ICourseService {
 
     @Resource
-    private RabbitTemplate rabbitTemplate;
-
-    @Resource
     private UserClient userClient;
-
-    @Resource
-    private ObjectMapper objectMapper;
 
     @Resource
     private CrawlerConfig crawlerConfig;
@@ -57,13 +48,12 @@ public class CourseServiceImpl extends ServiceImpl<SignLogMapper, SignLog> imple
         if (userInfo == null || userInfo.getStudentId() == null || !userInfo.getVerified()) {
             return Result.error("用户未认证");
         }
-        return Result.success(courseCrawlers.get(crawlerConfig.getChannel()).getScheduleByDate(userInfo.getStudentId(), date));
+        return Result.success(getScheduleByDate(userInfo.getStudentId(), date));
     }
 
     @Override
     public ScheduleVO getScheduleByDate(String studentId, LocalDate date) {
-        Map res = (Map) rabbitTemplate.convertSendAndReceive("course.direct", "course.query", new CourseQueryDTO(studentId, date));
-        return objectMapper.convertValue(res, ScheduleVO.class);
+        return courseCrawlers.get(crawlerConfig.getChannel()).getScheduleByDate(studentId, date);
     }
 
     @Override
