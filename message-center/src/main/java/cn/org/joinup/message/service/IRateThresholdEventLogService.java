278e282a358edcd0eecefb9a2b7a734cf67e09ca
raochenxuan <raochenxuan@bytedance.com>
2026-01-21 14:48:50 +0800

    feat(monitor): add event log persistence for rate threshold events.

---
+++ message-center/src/main/java/cn/org/joinup/message/service/IRateThresholdEventLogService.java
@@ -0,0 +1,7 @@
+package cn.org.joinup.message.service;
+
+import cn.org.joinup.message.domain.po.RateThresholdEventLog;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface IRateThresholdEventLogService extends IService<RateThresholdEventLog> {
+}
